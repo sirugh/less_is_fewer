c1d4b3e9bc731b985c4fd061073313bf208d4833
Stephen Rugh <stephen.rugh@bazaarvoice.com>
2018-10-06 11:25:48 -0500

    Add colors scene

---
+++ src/scenes/colors.js
@@ -0,0 +1,16 @@
+// The colors level
+export default class Colors extends Phaser.Scene {
+  constructor() {
+    super({
+      key: 'colors',
+      active: false
+    })
+  }
+  preload () {
+
+  }
+
+  create () {
+
+  }
+}
