6cd8e1eb26da482c77a1f621e7004b5fc5e76038
Stephen Rugh <stephen.rugh@bazaarvoice.com>
2018-10-05 21:42:19 -0500

    Webpack n babel

--- src/engine/main.js
+++ src/engine/main.js
@@ -1,1 +1,28 @@
-// main mechanics here
+import 'phaser';
+import pkg from 'phaser/package.json';
+
+const width = 800;
+const height = 600;
+
+const config = {
+  width,
+  height,
+  type: Phaser.AUTO,
+  scene: { preload, create },
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+
+}
+
+function create() {
+  const centerX = width / 2;
+  const centerY = height / 2;
+  const welcomeMessage = `Welcome to Phaser ${pkg.version}`;
+
+  this.add
+    .text(centerX, centerY * 0.8, welcomeMessage, { font: "bold 19px Arial", fill: "#fff" })
+    .setOrigin(0.5, 0.5);
+}
