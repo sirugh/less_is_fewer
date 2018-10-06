dde4d86051d2601ed56b33f26e71e04c72fe88e7
Stephen Rugh <stephen.rugh@bazaarvoice.com>
2018-10-06 18:41:07 -0500

    Rename gravity to 2

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -1,7 +1,13 @@
 import Boot from './boot';
-import Gravity from './gravity';
-import Colors from './colors';
+import Level1 from './1';
+import Level2 from './2';
 import Level3 from './3';
 import End from './end';
 
-export default [Boot, Gravity, Colors, Level3, End];
+export default [
+  Boot,
+  Level1,
+  Level2,
+  Level3,
+  End
+];
