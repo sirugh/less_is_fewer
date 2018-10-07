8da020ad6ee46ba0c05c0de471a00ca13b040a27
Dan Heberden <GIT_EMAIL_HERE>
2018-10-06 19:00:43 -0500

    i didnt mean it

--- src/scenes/index.js
+++ src/scenes/index.js
@@ -2,7 +2,7 @@ import Boot from './boot';
 import Level1 from './1';
 import Level2 from './2';
 import Level3 from './3';
-import Dead from './3';
+import Dead from './dead';
 import End from './end';
 
 export default [
