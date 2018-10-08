98f4564dfd268be232b2a54c383a50ff1e5eddcf
Stephen Rugh <stephen.rugh@bazaarvoice.com>
2018-10-08 14:04:14 -0500

    Fix productionized asset build

--- webpack.config.js
+++ webpack.config.js
@@ -2,53 +2,73 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-const config = {
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    modules: ['src', 'node_modules'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: 'file-loader',
-      },
-      {
-        test: /\.(vert|frag)$/,
-        use: 'raw-loader',
-      },
-    ],
-  },
-  stats: 'minimal',
-  devtool: 'source-map',
-  performance: {
-    hints: false,
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      CANVAS_RENDERER: true,
-      WEBGL_RENDERER: true,
-    }),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-    }),
-    new CopyWebpackPlugin([
-      { from: 'assets', to: 'assets' }
-    ])
-  ],
-  devServer: {
-    port: 8080,
+module.exports = (env, argv) => {
+  const config = {
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+      modules: ['src', 'node_modules'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|jpg|gif)$/,
+          use: 'file-loader',
+        },
+        {
+          test: /\.(vert|frag)$/,
+          use: 'raw-loader',
+        },
+      ],
+    },
     stats: 'minimal',
-  },
-};
+    performance: {
+      hints: false,
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        CANVAS_RENDERER: true,
+        WEBGL_RENDERER: true,
+      }),
+      new HtmlWebpackPlugin({
+        template: 'src/index.html',
+      }),
+      new CopyWebpackPlugin([
+        { from: 'assets', to: 'assets' }
+      ])
+    ],
+    devServer: {
+      port: 8080,
+      stats: 'minimal',
+    },
 
-module.exports = config;
+  };
+
+  if (argv.mode === 'development') {
+    config.devtool = 'source-map'
+  }
+  else {
+    config.optimization = {
+      minimizer: [
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            mangle: {
+              reserved: ['Level']
+            },
+          }
+        })
+      ]
+    }
+  }
+
+  return config;
+}
